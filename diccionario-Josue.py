aef873904cad52bfe1ac0dec89c57a7fdcb35bed
USUARIO02-PC\Usuario01 <josuericardocando@gmail.com>
2016-11-16 13:42:10 -0500

    Trabajo en clases sobre diccionario

---
+++ diccionario-Josue.py
@@ -0,0 +1,5 @@
+diccionario = {"Juan":"202112345","Ramon":"202213456","Pedro":"202312345","Mishel":"202412345","Maria":"202512345"}
+print("Antiguo numero: ",diccionario["Mishel"])
+del diccionario["Mishel"]
+diccionario["Mishel"]= "0998116988"
+print("Nuevo nuemro: ",diccionario["Mishel"])
