7de22b1bbef5b856687807fe6869ce4a4b1f808d
Usuario01-PC\Usuario01 <mishelcenteno96@gmail.com>
2016-11-16 13:50:20 -0500

    Cambio de eliminacion

--- diccionario-Josue.py
+++ diccionario-Josue.py
@@ -1,5 +1,5 @@
 diccionario = {"Juan":"202112345","Ramon":"202213456","Pedro":"202312345","Mishel":"202412345","Maria":"202512345"}
-print("Antiguo numero: ",diccionario["Mishel"])
-del diccionario["Mishel"]
-diccionario["Mishel"]= "0998116988"
-print("Nuevo nuemro: ",diccionario["Mishel"])
+print("Antiguo numero: ",diccionario["Ramon"])
+del diccionario["Ramon"]
+diccionario["Ramon"]= "0998116988"
+print("Nuevo nuemro: ",diccionario["Ramon"])
