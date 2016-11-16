f3ec5a6a671ed810c7067c3f123f8acdb0af51d3
Usuario01-PC\Usuario01 <mishelcenteno96@gmail.com>
2016-11-16 13:44:01 -0500

    Practica de diccionario

---
+++ dicionario_MishelCenteno.py
@@ -0,0 +1,9 @@
+amigos={'mafer':'2155','josue':'878441','stalin':'21454551','david':'485745245','edison':'78444'}
+print('directorio:',amigos)
+
+amigos['pedro']='45654'
+
+del amigos['josue']
+
+amigos['josue']='0984254187'
+print(amigos)
